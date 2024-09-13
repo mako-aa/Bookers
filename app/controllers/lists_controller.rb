bfb5f2eeecf8c8862089073f6037c8ee4904aaf2
EC2 Default User <ec2-user@ip-172-31-5-182.us-east-2.compute.internal>
2024-09-13 10:00:36 +0000

    [Finish]add flash messages

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,25 +1,26 @@
 class ListsController < ApplicationController
-  def new
+  def index
     #Viewへわたす変数
     @list = List.new
+    @lists = List.all
   end
   
   def create
     #データを受け取り新規登録するため
     @list = List.new(list_params)
+  
     #データが入力されているか
     if @list.save
+      #フラッシュメッセージ
+      flash[:notice] = "Book was successfully created."
       redirect_to list_path(@list.id)
-    else
-      render :new
+    else 
+      @lists = List.all
+      render :index
     end
     
   end
-  
-  def index
-    #すべてのデータを取得
-    @lists = List.all
-  end
+
   
   def show
     #データを1件取得
